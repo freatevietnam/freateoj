82d9f1b1cbf5da1aecccd92b58a1d09f874d4063
nomoka <nomoka@users.noreply.github.com>
2026-07-10 06:27:33 +0000

    Fix status view - pass correct context variables to templates

--- judge/views/status.py
+++ judge/views/status.py
@@ -1,21 +1,39 @@
-from django.http import JsonResponse
+from collections import defaultdict
+from functools import partial
+
+from django.http import HttpResponseBadRequest, HttpResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
+from django.utils.translation import gettext as _
+from packaging import version
 
-from judge.models import Language
+from judge.models import Judge, Language, RuntimeVersion
+
+
+def get_judges(request):
+    if request.user.is_superuser or request.user.is_staff:
+        return True, Judge.objects.order_by('-online', 'name')
+    else:
+        return False, Judge.objects.filter(online=True)
 
 
 def status_all(request):
-    languages = Language.objects.filter(runtimeversion__isnull=False).distinct()
+    see_all, judges = get_judges(request)
+    languages = Language.objects.filter(runtimeversion__isnull=False).distinct().order_by('key')
     
-    context = {
+    return render(request, 'status/status.html', {
+        'judges': judges,
+        'runtime_version_data': Judge.runtime_versions(),
+        'see_all_judges': see_all,
         'languages': languages,
-    }
-    
-    return render(request, 'status/status.html', context)
+    })
 
 
 def status_table(request):
-    """AJAX endpoint to get updated judge status table."""
-    html = render_to_string('status/judge-status-table.html')
-    return JsonResponse({'html': html})
+    see_all, judges = get_judges(request)
+    html = render_to_string('status/judge-status-table.html', {
+        'judges': judges,
+        'runtime_version_data': Judge.runtime_versions(),
+        'see_all_judges': see_all,
+    })
+    return HttpResponse(html)
