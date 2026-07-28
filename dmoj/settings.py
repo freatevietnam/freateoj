4fbe8d9ba1c3db030983446ca65ef9931f664eb2
nmkdeveloper <nguyenminhkhoi.nmk.dev@gmail.com>
2026-07-28 20:13:56 +0700

    fix: hardcode character limit via Jinja2 truncate for all homepage titles

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -843,7 +843,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'resources'),
 ]
 STATIC_URL = '/static/'
-STATIC_VERSION = '0.6'
+STATIC_VERSION = '0.7'
 STATICFILES_STORAGE = 'dmoj.storage.ManifestStaticFilesStorageSafe'
 
 # Define a cache
