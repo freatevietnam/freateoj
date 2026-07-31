1813757d1729eb6cc15f8639386de60df99bf314
nmkdeveloper <nguyenminhkhoi.nmk.dev@gmail.com>
2026-07-31 17:05:10 +0700

    add horizontal padding to page-container

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -844,7 +844,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'resources'),
 ]
 STATIC_URL = '/static/'
-STATIC_VERSION = '0.15'
+STATIC_VERSION = '0.16'
 STATICFILES_STORAGE = 'dmoj.storage.ManifestStaticFilesStorageSafe'
 
 # Define a cache
