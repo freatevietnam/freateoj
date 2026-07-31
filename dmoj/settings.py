4435cf900be4b22b98326ff2b489ed763b98f28b
nmkdeveloper <nguyenminhkhoi.nmk.dev@gmail.com>
2026-07-31 16:59:21 +0700

    replace Segoe UI with Times New Roman, replace status FA icons with SVG

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -844,7 +844,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'resources'),
 ]
 STATIC_URL = '/static/'
-STATIC_VERSION = '0.14'
+STATIC_VERSION = '0.15'
 STATICFILES_STORAGE = 'dmoj.storage.ManifestStaticFilesStorageSafe'
 
 # Define a cache
