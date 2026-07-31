9073a6808af3ae226f386d6c89cfac702341a133
nmkdeveloper <nguyenminhkhoi.nmk.dev@gmail.com>
2026-07-31 08:11:17 +0700

    bump STATIC_VERSION to 0.9

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -844,7 +844,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'resources'),
 ]
 STATIC_URL = '/static/'
-STATIC_VERSION = '0.8'
+STATIC_VERSION = '0.9'
 STATICFILES_STORAGE = 'dmoj.storage.ManifestStaticFilesStorageSafe'
 
 # Define a cache
